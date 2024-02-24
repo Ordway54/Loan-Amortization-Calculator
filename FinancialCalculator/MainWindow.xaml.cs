4007d3f9218ed9b68a19abab6da123a198463654
Justin Ordway <justin.ordway@aol.com>
2024-02-24 12:49:43 -0500

    Add project files.

---
+++ FinancialCalculator/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+
+namespace FinancialCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Dictionary<int, double> GetAmortizationSchedule()
+        {
+            Dictionary<int, double> myDict = new Dictionary<int, double>
+            {
+                {1, 1.23},
+                {2, 1.24}
+            };
+            return myDict;
+        }
+    }
+}
